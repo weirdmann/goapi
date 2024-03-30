362b9f1b343a60ab10d84db73ae7c557182a43e5
weirdmann <hubwe@pm.me>
2024-03-30 21:25:14 +0100

    more go-like style applied; dialer implemented

--- main.go
+++ main.go
@@ -11,27 +11,42 @@ import (
 	"sync"
 	"time"
 
-	"github.com/weirdmann/go-graceful-shutdown"
+	"strings"
+
+	gs "github.com/weirdmann/go-graceful-shutdown"
 
 	"github.com/beevik/guid"
 )
 
-var main_WG sync.WaitGroup
+var wg sync.WaitGroup
 
 func main() {
 	logger := log.Default()
 	logger.Println("Starting...")
 	tcp_context, cancel := context.WithCancel(context.Background())
+	go gs.GracefulShutdown(logger, cancel)
 
 	tcp := NewTcpEndpoint("0.0.0.0", 2137, &tcp_context, logger)
+	recv, send := tcp.Listen()
+	tcp_d := NewTcpEndpoint("127.0.0.1", 2139, &tcp_context, logger)
+	rd, sd := tcp_d.Dial()
 
-	main_WG.Add(1)
-	go tcp.Listen()
-
-	logger.Println("Started.")
-	go graceful_shutdown.GracefulShutdown(logger, cancel)
-	main_WG.Wait()
-
+	handle := func(recv_chan *chan []byte, send_chan *chan []byte) {
+		defer wg.Done()
+		if recv_chan == nil || send_chan == nil {
+			return
+		}
+		for r := range *recv_chan {
+			log.Printf("[INFO] Received: %s", r)
+			str := string(r[:])
+			str = strings.TrimSpace(str)
+			*send_chan <- r
+		}
+	}
+	wg.Add(2)
+	go handle(recv, send)
+	go handle(rd, sd)
+	wg.Wait()
 }
 
 // ---- Tcp Endpoint ---- //
@@ -45,6 +60,9 @@ type TcpEndpoint struct {
 	listener      net.Listener
 	dialer        net.Dialer
 	logger        *log.Logger
+	peers         map[string]*TcpPeer
+	recv_chan     chan []byte
+	send_chan     chan []byte
 }
 
 func NewTcpEndpoint(ipAddr string, port uint16, ctx *context.Context, logger *log.Logger) *TcpEndpoint {
@@ -54,71 +72,141 @@ func NewTcpEndpoint(ipAddr string, port uint16, ctx *context.Context, logger *lo
 		config_IPAddr: net.ParseIP(ipAddr),
 		config_Port:   port,
 		logger:        logger,
+		peers:         make(map[string]*TcpPeer),
 	}
+}
+
+func (this *TcpEndpoint) ListenerSoftReset() (*chan []byte, *chan []byte) {
+	this.listener.Close()
+	for _, tcpPeer := range this.peers {
+		tcpPeer.Close()
+	}
+	return this.Listen()
 }
 
 func (this *TcpEndpoint) ListenerStop() {
 	select {
 	case <-(*this.ctx).Done():
 		this.logger.Printf("Stopping TCP Listener %s", this.listener.Addr())
+		for _, tcpPeer := range this.peers {
+			tcpPeer.Close()
+		}
 		this.listener.Close()
+		close(this.recv_chan)
+		close(this.send_chan)
 	}
 }
 
 func (this *TcpEndpoint) DialerStop() {
-  select {
+	select {
 
-    case <- (*this.ctx).Done():
-      this.logger.Printf("Stopping TCP Dialer: %s", this.dialer.LocalAddr)
-  }
+	case <-(*this.ctx).Done():
+		this.logger.Printf("Stopping TCP Dialer: %s", this.dialer.LocalAddr)
+		for _, tcpPeer := range this.peers {
+			tcpPeer.Close()
+		}
+		close(this.recv_chan)
+		close(this.send_chan)
+	}
 }
 
-func (this *TcpEndpoint) Listen() {
+// do it the "GO" way:
+// Listen should not be started as a goroutine
+// but start its own goroutine and return a channel that
+// will receive the packets
+func (this *TcpEndpoint) Listen() (*chan []byte, *chan []byte) {
+	this.recv_chan = make(chan []byte, 64)
+	this.send_chan = make(chan []byte, 64)
 
-	defer main_WG.Done()
 	var err error
 
 	this.listener, err = net.Listen("tcp4", fmt.Sprintf("%s:%d", this.config_IPAddr.String(), this.config_Port))
 	if err != nil {
 		this.logger.Println(err)
-		return
+		return nil, nil
 	}
 
 	go this.ListenerStop()
-	for {
-		// Accept incoming connections
-		conn, err := this.listener.Accept()
-		if errors.Is(err, net.ErrClosed) {
-			return
-		}
-		if err != nil {
-			this.logger.Println("Error:", err)
-			return
-		}
+	go func() {
+		for {
+			// Accept incoming connections
+			conn, err := this.listener.Accept()
+			if errors.Is(err, net.ErrClosed) {
+				return
+			}
+			if err != nil {
+				this.logger.Println("Error:", err)
+				return
+			}
 
-		tcpPeer, _ := NewTcpPeer(conn, this.logger)
-		// Handle client connection in a goroutine
-		this.logger.Printf("New TCP Peer connected: %s", tcpPeer.Conn.RemoteAddr())
-		go tcpPeer.Receive()
-	}
+			tcpPeer, _ := NewTcpPeer(conn, this.logger)
+			this.peers[tcpPeer.Id.String()] = tcpPeer
+			// Handle client connection in a goroutine
+			this.logger.Printf("New TCP Peer connected: %s", tcpPeer.Conn.RemoteAddr())
+			tp_recv_chan := tcpPeer.Receive(func() { delete(this.peers, tcpPeer.Id.String()) })
+			go func() {
+				for received := range *tp_recv_chan {
+					this.recv_chan <- received
+				}
+			}()
+		}
+	}()
+	// Sender coroutine - spread the sent data between all peers
+	go func() {
+		for to_send := range this.send_chan {
+			for _, peer := range this.peers {
+				peer.send_chan <- to_send
+			}
+		}
+	}()
+	return &this.recv_chan, &this.send_chan
 }
 
-func (this *TcpEndpoint) Dial() {
+func (this *TcpEndpoint) Dial() (*chan []byte, *chan []byte) {
+	this.send_chan = make(chan []byte, 64)
+	this.recv_chan = make(chan []byte, 64)
+	go this.DialerStop()
 
-  this.dialer.Dial("tcp4", fmt.Sprintf("%s:%d", this.config_IPAddr, this.config_Port))
+	addr := fmt.Sprintf("%s:%d", this.config_IPAddr, this.config_Port)
+	var conn net.Conn
+	this.dialer.Deadline.Add(5 * time.Second)
+	conn, err := this.dialer.DialContext(*this.ctx, "tcp4", addr)
 
+	if err != nil {
+		this.logger.Printf("TCP Client connection attempt failed for %s, %s", addr, err)
+		return nil, nil
+	}
 
+	this.logger.Printf("TCP Client connected to %s", conn.RemoteAddr())
+
+	tcpPeer, _ := NewTcpPeer(conn, this.logger)
+	this.peers[tcpPeer.Id.String()] = tcpPeer
+	tp_recv_chan := tcpPeer.Receive(func() { delete(this.peers, tcpPeer.Id.String()) })
+	go func() {
+		for received := range *tp_recv_chan {
+			this.recv_chan <- received
+		}
+	}()
+	// Sender coroutine - spread the sent data between all peers
+	go func() {
+		for to_send := range this.send_chan {
+			for _, peer := range this.peers {
+				peer.send_chan <- to_send
+			}
+		}
+	}()
+	return &this.recv_chan, &this.send_chan
 }
 
 // --------------------- //
 
 // ---- Tcp Peer ---- //
-
+// TcpPeer
 type TcpPeer struct {
 	Id        guid.Guid
 	Conn      net.Conn
-	recv_chan chan []byte
-	send_chan chan []byte
+	recv_chan chan []byte // TcpPeer receives data and sends it to the recv_chan
+	send_chan chan []byte // TcpPeer reads the send_chan and sends the data to its partner
 	logger    *log.Logger
 }
 
@@ -127,41 +215,60 @@ func NewTcpPeer(conn net.Conn, logger *log.Logger) (*TcpPeer, error) {
 		Id:        *guid.New(),
 		Conn:      conn,
 		recv_chan: make(chan []byte, 64),
+		send_chan: make(chan []byte, 64),
 		logger:    logger,
 	}
 
 	return n, nil
 }
 
+func (this *TcpPeer) Close() {
+	close(this.recv_chan)
+	close(this.send_chan)
+	this.Conn.Close()
+}
+
 func (this *TcpPeer) GetConn() net.Conn {
 	return this.Conn
 }
 
-func (this *TcpPeer) Receive() {
-	buffer := make([]byte, 1024)
-	for {
-		// Read data from the client
-		n, err := this.Conn.Read(buffer)
-
-		if errors.Is(err, os.ErrDeadlineExceeded) {
-			UpdateTimeout(this.Conn, 5)
-			continue
-		}
-
-		if errors.Is(err, io.EOF) {
-			this.logger.Printf("[INFO] Client disconnected: %s\n", this.Conn.RemoteAddr())
-			return
-		}
-
-		if err != nil {
-			this.logger.Println("[ERRR] Error:", err)
-			return
-		}
-
-		// Process and use the data (here, we'll just print it)
-		this.logger.Printf("[VERB] Received: %s", buffer[:n])
-		UpdateTimeout(this.Conn, 5)
+func (this *TcpPeer) Send() {
+	for to_send := range this.send_chan {
+		this.Conn.Write(to_send)
 	}
+}
+
+func (this *TcpPeer) Receive(on_disconnect func()) *chan []byte {
+	buffer := make([]byte, 1024)
+	go func() {
+		for {
+			// Read data from the client
+			n, err := this.Conn.Read(buffer)
+
+			if errors.Is(err, os.ErrDeadlineExceeded) {
+				UpdateTimeout(this.Conn, 5)
+				continue
+			}
+			if errors.Is(err, io.EOF)   {
+				this.logger.Printf("[INFO] Client disconnected: %s\n", this.Conn.RemoteAddr())
+				this.Close()
+				on_disconnect()
+				return
+			}
+      if errors.Is(err, net.ErrClosed) {
+        on_disconnect()
+        return
+      }
+			if err != nil {
+				this.logger.Println("[ERRR] Error:", err)
+				return
+			}
+			this.recv_chan <- buffer[:n]
+			UpdateTimeout(this.Conn, 5)
+		}
+	}()
+	go this.Send()
+	return &this.recv_chan
 }
 
 // ----------------- //
@@ -170,47 +277,3 @@ func (this *TcpPeer) Receive() {
 func UpdateTimeout[C net.Conn](conn C, seconds int) {
 	conn.SetDeadline(time.Now().Add(time.Second * time.Duration(seconds)))
 }
-
-/*
-func handleClient(conn net.Conn) {
-
-	tp, err := NewTcpPeer(conn)
-	if err != nil {
-		this.logger.Println("Error creating a TCP Peer")
-		panic(0)
-	}
-
-	this.logger.Println(tp.Id.String())
-	this.logger.Println(tp.GetConn().RemoteAddr())
-	UpdateTimeout(conn, 5)
-	defer conn.Close()
-
-	// Create a buffer to read data into
-	buffer := make([]byte, 1024)
-
-	logger.Printf("[INFO] Client connected: %s\n", conn.RemoteAddr())
-	for {
-		// Read data from the client
-		n, err := conn.Read(buffer)
-
-		if errors.Is(err, os.ErrDeadlineExceeded) {
-			UpdateTimeout(conn, 5)
-			continue
-		}
-
-		if errors.Is(err, io.EOF) {
-			logger.Printf("[INFO] Client disconnected: %s\n", conn.RemoteAddr())
-			return
-		}
-
-		if err != nil {
-			logger.Println("[ERRR] Error:", err)
-			return
-		}
-
-		// Process and use the data (here, we'll just print it)
-		logger.Printf("[VERB] Received: \n%s\n", buffer[:n])
-		UpdateTimeout(conn, 5)
-	}
-}
-*/
